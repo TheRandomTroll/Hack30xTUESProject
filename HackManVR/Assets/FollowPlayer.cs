83ff2c0e96e23dfe42ddfd89cb8b7e75656f787d
Gdemirev01 <gdemirev78@gmail.com>
2018-03-15 23:33:23 +0200

    Camera

---
+++ HackManVR/Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Vector3 offset;
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+    }
+}
