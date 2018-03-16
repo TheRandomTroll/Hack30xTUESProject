482090cb79fc8c3f9b3250cad059d597aa3820dd
Krasi <krasi2405@gmail.com>
2018-03-16 16:47:40 +0200

    Bug Fix

--- HackManVR/Assets/CameraVRRay.cs
+++ HackManVR/Assets/CameraVRRay.cs
@@ -6,19 +6,22 @@ public class CameraVRRay : MonoBehaviour {
 
     [SerializeField]
     GameObject point;
-
-	void Start () {
-    }
+    
 
 
-	void Update () {
+	void FixedUpdate () {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
         Debug.DrawRay(transform.position, transform.forward * 50, Color.yellow);
         if (Physics.Raycast(ray, out hit, 50f))
         {
             HasTriggered isTriggered = hit.transform.GetComponent<HasTriggered>();
-            if (!isTriggered) return;
+            if (!isTriggered)
+            {
+                Debug.Log("No HasTriggered script!");
+                return;
+            }
+
             isTriggered.Trigger();
             Debug.Log("Hit " + hit.point);
             point.transform.position = hit.point;
