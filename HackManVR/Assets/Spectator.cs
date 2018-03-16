234a5b334d362a15d08b4d6ff498055fab9b7a36
Gdemirev01 <gdemirev78@gmail.com>
2018-03-16 13:23:26 +0200

    User Creator Camera done

---
+++ HackManVR/Assets/Spectator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spectator : MonoBehaviour
+{
+    public int speed = 20;
+
+    void Update()
+    {
+        //For the following 'if statements' don't include 'else if', so that the user can press multiple buttons at the same time
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += this.transform.forward * this.speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S))
+            this.transform.position += this.transform.forward * -1 * this.speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += this.transform.right * this.speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position += this.transform.right * -1 * this.speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+            this.transform.position += this.transform.up * this.speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            this.transform.position += this.transform.up * -1 * this.speed * Time.deltaTime;
+
+    }
+}
