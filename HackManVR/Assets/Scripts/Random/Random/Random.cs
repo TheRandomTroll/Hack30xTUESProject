065dbb8232cdab74f7d8b43f181e6c552f2370fd
kgbspijonin <37420792+kgbspijonin@users.noreply.github.com>
2018-03-16 14:22:24 +0200

    Add files via upload

---
+++ HackManVR/Assets/Scripts/Random/Random/Random.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+public class RNGCSP
+{
+    private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+    public static byte RNG(byte max)
+    {
+        byte[] rnd = new byte[1];
+        rngCsp.GetBytes(rnd);
+        return (byte)(rnd[0] % max);
+    }
+}
+
+public class Random : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
