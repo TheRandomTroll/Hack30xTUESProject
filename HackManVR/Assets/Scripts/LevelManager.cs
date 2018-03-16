5dbdc6a69d7f09f341cd4b1e5d31b62213a06fb2
Krasi <krasi2405@gmail.com>
2018-03-16 10:30:17 +0200

    Start menu

---
+++ HackManVR/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadNextLevel()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
