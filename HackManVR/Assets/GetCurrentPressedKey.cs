05d7a55fe24cd2ffbb098196fdc55502e7c16106
Krasi <krasi2405@gmail.com>
2018-03-16 15:22:05 +0200

    Debug Stuff.

---
+++ HackManVR/Assets/GetCurrentPressedKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCurrentPressedKey : MonoBehaviour {
+
+    private void Start()
+    {
+        foreach (string str in Input.GetJoystickNames())
+        {
+            Debug.Log("Connected: " + str);
+        }
+    }
+
+
+    void Update () {
+        System.Array values = System.Enum.GetValues(typeof(KeyCode));
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKeyDown(code)) { Debug.Log(System.Enum.GetName(typeof(KeyCode), code)); }
+        }
+    }
+}
