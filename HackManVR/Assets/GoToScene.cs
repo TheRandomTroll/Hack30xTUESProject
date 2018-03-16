33d6ecbf15dd59930021286efa53596d5f95309e
Krasimir Georgiev <krasi2405@gmail.com>
2018-03-17 01:17:28 +0200

    Bug Fix and Added GoToScene script for UI.

--- HackManVR/Assets/GoToScene.cs
+++ HackManVR/Assets/GoToScene.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(HasTriggered))]
 public class GoToScene : MonoBehaviour {
+
+    [SerializeField]
+    private int sceneIndex = -1;
     
     [SerializeField]
+    private string sceneName;
+
+    private HasTriggered trigger;
 
 
-	void Start () {
-		
-	}
-	
+    // TODO: Change for console.
+    KeyCode leftClick = KeyCode.Mouse0;
+
+
+    void Start()
+    {
+        trigger = GetComponent<HasTriggered>();
+    }
+
 
 	void Update () {
-		
+        if(trigger.GetTrigger() && Input.GetKey(leftClick))
+        {
+            if(sceneIndex > 0)
+            {
+                LoadScene(sceneIndex);
+            }
+            else
+            {
+                LoadScene(sceneName);
+            }
+        }
 	}
+
+
+    private void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    private void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+
 }
