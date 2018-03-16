8d1834ee2e22d654ae1bcbc86d859a1dd602f236
kgbspijonin <37420792+kgbspijonin@users.noreply.github.com>
2018-03-16 14:14:48 +0200

    Add files via upload

---
+++ HackManVR/Assets/Scripts/Random/RandomLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLibrary : MonoBehaviour {
+
+    public struct IntVector2;
+    {
+    public Vector2 map = new Vector2(0, 0);
+    public int MAX = 50;
+    private int x;
+    private int y;
+    }
+
+    public void GenBorder();
+    {
+        
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
