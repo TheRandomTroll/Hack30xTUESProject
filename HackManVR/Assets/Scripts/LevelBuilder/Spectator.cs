4d5d567459a015953cc555eaa8adc24897711b89
Krasimir Georgiev <krasi2405@gmail.com>
2018-03-17 23:05:03 +0200

    Fix Spectator

--- HackManVR/Assets/Scripts/LevelBuilder/Spectator.cs
+++ HackManVR/Assets/Scripts/LevelBuilder/Spectator.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class Spectator : MonoBehaviour
 {
+    [SerializeField]
+    Camera camera;
+
     public int speed = 20;
 
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position += camera.transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += camera.transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         
         if (Input.GetButton("Backward"))
         {
-            transform.position += transform.up * speed * Time.deltaTime;
+            transform.position += camera.transform.up * speed * Time.deltaTime;
         }
 
         if(Input.GetButton("Forward"))
         {
 
-            transform.position += -transform.up * speed * Time.deltaTime;
+            transform.position += -camera.transform.up * speed * Time.deltaTime;
         }
-
-        /*
+        
         if (Input.GetKey(KeyCode.W))
-            this.transform.position += this.transform.forward * this.speed * Time.deltaTime;
+            transform.position += camera.transform.forward * speed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.S))
-            this.transform.position += this.transform.forward * -1 * this.speed * Time.deltaTime;
+            transform.position += camera.transform.forward * -1 * speed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.D))
-            this.transform.position += this.transform.right * this.speed * Time.deltaTime;
+            transform.position += camera.transform.right * speed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-            this.transform.position += this.transform.right * -1 * this.speed * Time.deltaTime;
+            transform.position += camera.transform.right * -1 * speed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
-            this.transform.position += this.transform.up * this.speed * Time.deltaTime;
+            transform.position += camera.transform.up * speed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftShift))
-            this.transform.position += this.transform.up * -1 * this.speed * Time.deltaTime;
-        */
+            transform.position += camera.transform.up * -1 * speed * Time.deltaTime;
     }
 }
