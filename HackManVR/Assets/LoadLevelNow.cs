a65b0810210740762ee64982a600392b235120fb
Krasimir Georgiev <krasi2405@gmail.com>
2018-03-18 04:35:55 +0200

    LoadLevel

---
+++ HackManVR/Assets/LoadLevelNow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelNow : MonoBehaviour {
+    
+	void Start () {
+        Invoke("LoadLevel", 0.5f);
+	}
+
+    public void LoadLevel()
+    {
+        FindObjectOfType<LoadLevel>().Load(0);
+    }
+}
