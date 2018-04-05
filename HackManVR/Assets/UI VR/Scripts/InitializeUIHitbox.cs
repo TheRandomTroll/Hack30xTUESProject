3074c91f91e998b598eb6b65d170f0f52b45a86b
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-06 00:55:10 +0300

    Promeni

---
+++ HackManVR/Assets/UI VR/Scripts/InitializeUIHitbox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class InitializeUIHitbox : MonoBehaviour {
+
+    private BoxCollider boxCollider;
+    private TextMesh textMesh;
+
+	void Start () {
+
+        textMesh = GetComponent<TextMesh>();
+        if (!textMesh) textMesh = gameObject.AddComponent<TextMesh>();
+        
+    }
+
+
+    void Update()
+    {
+        UpdateHitbox();
+    }
+
+
+    void UpdateHitbox()
+    {
+        BoxCollider[] boxColliders = GetComponents<BoxCollider>();
+        foreach (BoxCollider collider in boxColliders)
+            DestroyImmediate(collider);
+
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        Vector3 size = boxCollider.size;
+        size.z = 10;
+        boxCollider.size = size;
+    }
+}
