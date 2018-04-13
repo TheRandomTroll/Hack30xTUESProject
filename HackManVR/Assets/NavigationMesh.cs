f3922cf63d7d54d02b303fcc94e5b6754703916c
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-13 19:08:37 +0300

    Bazov dinamichen navmesh. Mnogo mnogo sbagqsano ama raboti donqkude

---
+++ HackManVR/Assets/NavigationMesh.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationMesh : MonoBehaviour {
+
+    [SerializeField] NavMeshSurface navMeshSurface; // TODO: remove serialize field!
+    
+	void Start () {
+        navMeshSurface = GetComponent<NavMeshSurface>();
+        navMeshSurface.BuildNavMesh();
+	}
+}
