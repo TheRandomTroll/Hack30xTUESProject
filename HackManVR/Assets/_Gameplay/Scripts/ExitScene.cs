f341dab77eb3fd1179f6a17913755ccef69084b6
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-24 21:20:26 +0300

    Fixove i sum svurzal scenovete edin s drug. Izpolzva se nov 'Back' button deto e na joystick button 12 (ps butonche na ps4) za vrushtane nazad vuv scenovete.

---
+++ HackManVR/Assets/_Gameplay/Scripts/ExitScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitScene : MonoBehaviour {
+
+    [SerializeField] string scene;
+    [SerializeField] string buttonName = "Back";
+    [SerializeField] KeyCode keycode = KeyCode.Escape;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(keycode) || Input.GetButtonDown(buttonName))
+        {
+            SceneManager.LoadScene(scene);
+        }
+	}
+}
