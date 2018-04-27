7050404bfefd31527934145228d4dc4cdf771ab0
Krasimir Georgiev <krasi2405@gmail.com>
2018-04-27 13:34:46 +0300

    Mapped to Dualshock4 Input + Starting custom input manager. [WIP]

---
+++ HackManVR/Assets/TestInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInput : MonoBehaviour {
+    
+	
+	void Update () {
+        System.Array values = System.Enum.GetValues(typeof(KeyCode));
+        foreach (KeyCode code in values)
+        {
+            if (Input.GetKeyDown(code)) { print(System.Enum.GetName(typeof(KeyCode), code)); }
+        }
+
+    }
+}
